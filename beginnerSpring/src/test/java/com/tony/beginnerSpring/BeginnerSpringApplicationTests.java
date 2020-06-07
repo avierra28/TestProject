e42d35b358f12652dd21ce5b2f2fe70b6982e52f
Tony <tvierra25@gmail.com>
2020-06-07 10:23:49 -0700

    Testing uploaded

---
+++ beginnerSpring/src/test/java/com/tony/beginnerSpring/BeginnerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.beginnerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
