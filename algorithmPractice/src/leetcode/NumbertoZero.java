d6cc3280a66c94847e5852907332acc5349d6155
Tony <tvierra25@gmail.com>
2020-06-11 10:12:19 -0700

    Number of steps to reduce number to 0.

---
+++ algorithmPractice/src/leetcode/NumbertoZero.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+public class NumbertoZero 
+{
+
+	public static void main(String[] args) 
+	{
+		// Start out with an integer.
+		int num = 14;
+		
+		// Call the function.
+		System.out.println(numCount(num));
+	}
+	
+	public static int numCount(int n)
+	{
+		// Declare a temporary integer and some counters.
+		int numofSteps = 0;
+		
+		// While loop.
+		while(n > 0)
+		{
+			// Do this as long as n is greater than zero.
+			if(n % 2 == 0)
+			{
+				// Meaning even. Divide by half.
+				n /= 2;
+				numofSteps++;
+				continue;
+			}
+			else
+			{
+				// Meaning its odd. Minus it by 1.
+				n -= 1;
+				numofSteps++;
+				continue;
+			}
+		}
+		
+		// Return the number of steps.
+		return numofSteps;
+	}
+}
