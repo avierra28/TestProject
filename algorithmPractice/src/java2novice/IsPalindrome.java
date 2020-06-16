80419a7f018e419d49ad0df9a3b0dd70fd3604e6
Tony <tvierra25@gmail.com>
2020-06-16 08:25:23 -0700

    Palindrome checker with a LL and stack.

---
+++ algorithmPractice/src/java2novice/IsPalindrome.java
@@ -0,0 +1,75 @@
+package java2novice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Stack;
+
+import java2novice.MiddleElement.Node;
+
+public class IsPalindrome 
+{
+	static class Node
+	{
+		int key;
+		Node nextNode;
+		public Node(int temp)
+		{
+			this.key = temp;
+		}
+	}
+	
+	private static Stack<Node> myStack;
+	
+	public static void main(String[] args) 
+	{
+		myStack = new Stack<Node>();
+		boolean flag = false;
+		
+		// Fill stack.
+		Node o1 = new Node(20);
+		Node o2 = new Node(21);
+		Node o3 = new Node(50);
+		Node o4 = new Node(21);
+		Node o5 = new Node(20);
+		
+		myStack.push(o1);
+		o1.nextNode = o2;
+		myStack.push(o2);
+		o2.nextNode = o3;
+		myStack.push(o3);
+		o3.nextNode = o4;
+		myStack.push(o4);
+		o4.nextNode = o5;
+		myStack.push(o5);
+		
+		System.out.println(isPalindrome(o1));
+	}
+	
+	public static boolean isPalindrome(Node head)
+	{
+		Node tempNode = head;
+		
+		while(tempNode.nextNode != null)
+		{
+			if(tempNode.key != myStack.pop().key) 
+				return Boolean.FALSE;
+			tempNode = tempNode.nextNode;
+		}
+		return Boolean.TRUE;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
