bc7934a6d621b2e8dd2fb126a7dbd9cad0fdeeb6
Tony <tvierra25@gmail.com>
2020-06-16 18:05:46 -0700

    Reverse number using mathematical trick.

---
+++ algorithmPractice/src/java2novice/ReverseNumber.java
@@ -0,0 +1,28 @@
+package java2novice;
+
+public class ReverseNumber 
+{
+
+	public static void main(String[] args) 
+	{
+		// Declare integer.
+		int num = 154;
+		
+		// Reverse the integer.
+		reverseInt(num);
+	}
+	
+	public static void reverseInt(int n)
+	{
+		int temp = 0;
+		
+		while(n > 0)
+		{
+			temp = (temp * 10) + (n % 10);
+			n /= 10;
+		}
+		
+		System.out.println(temp);
+	}
+
+}
