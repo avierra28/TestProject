b5be54f12f864b78cd2ccefc0f39a88d7e0dfe22
Tony <tvierra25@gmail.com>
2020-06-17 10:53:47 -0700

    Binary tree implementation.

---
+++ algorithmPractice/src/java2novice/BTImplementation.java
@@ -0,0 +1,120 @@
+package java2novice;
+
+public class BTImplementation {
+
+	// Define a BT variable.
+	private static BinaryTree root;
+	
+	public static void main(String[] args) 
+	{
+		BTImplementation obj1 = new BTImplementation();
+		
+		// Add
+		obj1.insert(50);
+		obj1.insert(60);
+		obj1.insert(40);
+		obj1.insert(12);
+		obj1.insert(34);
+		obj1.insert(87);
+		obj1.insert(39);
+		obj1.insert(90);
+		
+		displayTree(root);
+	}
+
+	// Define an isempty.
+	public boolean isEmpty(BinaryTree r)
+	{
+		if(r == null)
+			return true;
+		else {
+			return false;
+		}
+	}
+	
+	// Define an add value.
+	public void insert(Integer key)
+	{
+		// Check if empty.
+		System.out.println("Inputing key:" + key);
+		
+		if(root == null)
+		{
+			// Meaning there is nothing in the tree.
+			this.root = new BinaryTree(key);
+			System.out.println(key + " inserted into the tree.");
+		}
+		else {
+			// Call the other function to insert the node.
+			insertNode(this.root, key);
+			System.out.println(key + " inserted into the tree.");
+		}
+	}
+	
+	// Define insernode.
+	public BinaryTree insertNode(BinaryTree r, Integer data)
+	{
+		// Create a holder node.
+		BinaryTree tempBinaryTree = null;
+		
+		if(r.getDataInteger() >= data)
+		{
+			// Check to see if thats null.
+			if(r.getLeft() == null)
+			{
+				// Create new instance.
+				r.setLeft(new BinaryTree(data));
+				return r.getLeft();
+			}
+			else {
+				tempBinaryTree = r.getLeft();
+			}
+		}
+		else {
+			
+			if(r.getRight() == null)
+			{
+				// Create new instance.
+				r.setRight(new BinaryTree(data));
+				return r.getRight();
+			}
+			else {
+				tempBinaryTree = r.getRight();
+			}
+		}
+		
+		// Do recursive call.
+		return insertNode(tempBinaryTree, data);
+		
+	}
+	
+	// Display the tree.
+	public static void displayTree(BinaryTree r)
+	{
+		if(r != null)
+		{
+			displayTree(r.getLeft());
+			System.out.println(r.getDataInteger());
+			displayTree(r.getRight());
+		}
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ algorithmPractice/src/java2novice/BinaryTree.java
@@ -0,0 +1,47 @@
+package java2novice;
+
+public class BinaryTree {
+	
+	// Define our attributes for our class.
+	private BinaryTree left, right;
+	private Integer dataInteger;
+	
+	
+	public BinaryTree(Integer dataInteger) {
+		super();
+		this.dataInteger = dataInteger;
+	}
+
+	public BinaryTree getLeft() {
+		return left;
+	}
+
+
+	public void setLeft(BinaryTree left) {
+		this.left = left;
+	}
+
+
+	public BinaryTree getRight() {
+		return right;
+	}
+
+
+	public void setRight(BinaryTree right) {
+		this.right = right;
+	}
+
+
+	public Integer getDataInteger() {
+		return dataInteger;
+	}
+
+
+	public void setDataInteger(Integer dataInteger) {
+		this.dataInteger = dataInteger;
+	}
+	
+	
+	
+
+}
