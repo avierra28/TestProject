41b6c196aac8f77e4d1c81c980923c5bce2a4aa1
Tony <tvierra25@gmail.com>
2020-06-16 18:06:01 -0700

    Reverse string using recursion.

---
+++ algorithmPractice/src/java2novice/ReverseString.java
@@ -0,0 +1,29 @@
+package java2novice;
+
+public class ReverseString 
+{
+	private static String masterString;
+	
+	public static void main(String[] args) 
+	{
+		masterString = "";
+		
+		// Declare string.
+		String str1 = new String("This is an example string.");
+
+		System.out.println(reverseString(str1));
+	}
+	
+	// Declare function to reverse.
+	public static String reverseString(String str)
+	{
+		if(str.length() == 1)
+			return str;
+		else {
+			masterString += str.charAt(str.length() - 1) + reverseString(str.substring(0, str.length() - 1));
+					
+			return masterString;
+		}
+	}
+
+}
