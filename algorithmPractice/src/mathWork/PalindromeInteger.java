3d97ae09092d54bcfaa2ea483db856ba8aa44f53
Tony <tvierra25@gmail.com>
2020-06-19 20:36:47 -0700

    Palindrome for a given integer checker.

---
+++ algorithmPractice/src/mathWork/PalindromeInteger.java
@@ -0,0 +1,30 @@
+package mathWork;
+
+public class PalindromeInteger {
+
+	public static void main(String[] args) {
+
+		int num = 15451;
+		
+		reverseInt(num);
+	}
+	
+	public static void reverseInt(int n) {
+		
+		int temp = 0, holder = n;
+		
+		while(holder > 0)
+		{
+			temp = (temp * 10) + (holder % 10);
+			holder = holder/10;
+		}
+		
+		
+		if(temp == n)
+			System.out.println("Integer is a palindrome");
+		else {
+			System.out.println("Integer is not a palindrome");
+		}
+	}
+
+}
