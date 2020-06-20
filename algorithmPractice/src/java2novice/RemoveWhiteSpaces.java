9d4ffd32f9c1f66156eb7c66b1af150fd0be229c
Tony <tvierra25@gmail.com>
2020-06-19 19:46:14 -0700

    Library work.

---
+++ algorithmPractice/src/java2novice/RemoveWhiteSpaces.java
@@ -0,0 +1,24 @@
+package java2novice;
+
+import java.util.Scanner;
+
+public class RemoveWhiteSpaces {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter string with spaces: ");
+		String str1 = scanner.nextLine();
+		
+		removeSpaces(str1);
+	}
+	
+	public static void removeSpaces(String s) {
+		
+		System.out.println(s.replaceAll(" ", ""));
+	}
+	
+	
+
+}
