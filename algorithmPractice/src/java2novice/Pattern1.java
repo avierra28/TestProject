fa0d9b20b78309e23f0297db48ffc335e053e340
Tony <tvierra25@gmail.com>
2020-06-19 19:45:49 -0700

    Pattern work

---
+++ algorithmPractice/src/java2novice/Pattern1.java
@@ -0,0 +1,37 @@
+package java2novice;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter number of rows: ");
+		int rows = scanner.nextInt();
+		
+		print(rows);
+	}
+	
+	public static void print(int numRows) {
+		int rowCount = 1;
+		
+		for (int i = numRows; i > 0; i--) {
+			
+			for (int j = 1; j <= i; j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j = 1; j <= rowCount; j++) {
+				System.out.print(rowCount + " ");
+			}
+			
+			System.out.println();
+			
+			rowCount++;
+		}
+		
+	}
+
+}
