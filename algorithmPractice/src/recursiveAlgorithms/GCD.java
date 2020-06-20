d768079b5b848733027c1658dc22ef6c1d432b8f
Tony <tvierra25@gmail.com>
2020-06-19 20:39:22 -0700

    Greatest Common Divisor between two numbers.

---
+++ algorithmPractice/src/recursiveAlgorithms/GCD.java
@@ -0,0 +1,27 @@
+package recursiveAlgorithms;
+
+import java.util.Scanner;
+
+public class GCD {
+
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Enter first number: ");
+		int num1 = scanner.nextInt();
+		System.out.print("Enter second number: ");
+		int num2 = scanner.nextInt();
+		
+		System.out.println("Greatest common divisor: " + func1(num1, num2));
+	}
+	
+	public static int func1(int n1, int n2) {
+		
+		if(n2 == 0)
+			return n1;
+		
+		return func1(n2, n1%n2);
+	}
+
+}
