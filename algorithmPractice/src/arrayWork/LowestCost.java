aaadec46ae90285cf80dce10cbf00c9b3faea191
Tony <tvierra25@gmail.com>
2020-06-19 20:27:20 -0700

    Finding the lowest cost in a given 2d array.

---
+++ algorithmPractice/src/arrayWork/LowestCost.java
@@ -0,0 +1,35 @@
+package arrayWork;
+
+public class LowestCost {
+
+	public static void main(String[] args)
+	{
+		// Declare a two dimensional array.
+		int[][] data = { {259, 770},
+						{448, 54},
+						{926, 667},
+						{184, 139},
+						{840, 118},
+						{577, 469} };
+						
+		
+		// Call the function.
+		System.out.println(getCost(data));
+		
+	}
+	
+	public static int getCost(int[][] arr)
+	{
+		int total = 0;
+		
+		// Iterate over the 2d array.
+		for (int i = 0; i < arr.length; i++) 
+			if(arr[i][0] > arr[i][1])
+				total += arr[i][1];
+			else
+				total += arr[i][0];
+			
+		return total;
+	}
+
+}
