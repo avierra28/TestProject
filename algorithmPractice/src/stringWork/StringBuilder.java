7616db1b2e0d3d4aaefef7fc64b12be17d14af13
Tony <tvierra25@gmail.com>
2020-06-20 23:02:07 -0700

    Stringbuilder program.

---
+++ algorithmPractice/src/stringWork/StringBuilder.java
@@ -0,0 +1,15 @@
+package stringWork;
+
+public class StringBuilder {
+
+	public static void main(String[] args) {
+		StringBuffer myBuffer = new StringBuffer();
+		
+		// Reverse string using built in stringbuilder
+		String str1 = "This string will be reversed.";
+		System.out.println(str1);
+		myBuffer.append(str1);
+		System.out.println(myBuffer.reverse());
+	}
+
+}
