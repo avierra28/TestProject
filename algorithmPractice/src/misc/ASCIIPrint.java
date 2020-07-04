fc97f103b188284b5493578e5cd80860285ed6a5
Tony <tvierra25@gmail.com>
2020-07-04 07:52:58 -0700

    Print out ascii characters.

---
+++ algorithmPractice/src/misc/ASCIIPrint.java
@@ -0,0 +1,19 @@
+package misc;
+
+public class ASCIIPrint {
+
+	public static void main(String[] args) {
+
+		int counter = 0;
+		char start = ' ';
+		
+		for (int i = (int)start; i <= 126; i++) {
+			System.out.print((char)i + " ");
+			counter++;
+			
+			if(counter % 16 == 0) 
+				System.out.println();
+		}
+	}
+
+}
