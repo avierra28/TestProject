a407b52a50f407c74ccb6619b8e63bbb134c2e4f
tony <tony@tony-Lenovo-ideapad-110-17ACL>
2021-07-01 16:21:51 -0700

    Making some changes here.
    Testing something out at the same time also. Hope this works.

---
+++ beginnerSpring/src/main/java/com/tony/beginnerSpring/Test.java
@@ -0,0 +1,11 @@
+package com.tony.beginnerSpring;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Employee myEmployee = new Employee(212, "Tony", "Vierra");
+		System.out.println(myEmployee);
+	}
+
+}
