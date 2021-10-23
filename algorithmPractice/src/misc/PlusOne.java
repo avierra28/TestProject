2f1eff3b256a384263656c13cc3a1e62b6ae36ee
Anthony <Anthony@DESKTOP-M3TF92Q.lan>
2021-10-22 22:38:04 -0400

    This is a test message. AV

---
+++ algorithmPractice/src/misc/PlusOne.java
@@ -0,0 +1,25 @@
+package misc;
+
+import java.util.Arrays;
+
+public class PlusOne {
+
+	public static void main(String[] args) {
+
+		// Initialize an array.
+		int[] array1 = { 1, 2, 3, 4 };
+
+		// Call the function to plus one.
+		plusone(array1);
+
+	}
+
+	public static void plusone(int[] arr) {
+
+		int n = arr.length - 1;
+		arr[n] = arr[n] + 1;
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
