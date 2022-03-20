ef56e53e2f0d5cc2ddd680f15c8e42c31de4a096
Anthony <tvierra25@gmail.com>
2022-03-20 18:55:22 -0400

    Cleaning up some code.

--- algorithmPractice/src/mathWork/DecimalBinary.java
+++ algorithmPractice/src/mathWork/DecimalBinary.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package mathWork;
 
 import java.util.Scanner;
@@ -36,60 +35,3 @@ public class DecimalBinary {
 	}
 
 }
-
-
-
-
-
-
-
-
-
-=======
-package mathWork;
-
-import java.util.Scanner;
-
-public class DecimalBinary {
-
-	public static void main(String[] args) {
-		/*
-		 * Testing this change on my new machine.
-		 */
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.print("Enter an integer: ");
-		int num = scanner.nextInt();
-
-		// Call function.
-		binaryConversion(num);
-
-		scanner.close();
-	}
-
-	public static void binaryConversion(int n) {
-		int[] tempArray = new int[30];
-		int counter = 0;
-
-		while (n > 0) {
-			// Fill our array.
-			tempArray[counter++] = n % 2;
-			n = n / 2;
-		}
-
-		for (int i = counter; i >= 0; i--)
-			System.out.print(tempArray[i]);
-
-	}
-
-}
-
-
-
-
-
-
-
-
-
->>>>>>> branch 'master' of https://github.com/avierra28/My-Repository
