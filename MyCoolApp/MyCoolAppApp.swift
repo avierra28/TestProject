9406ae40d6821baa6a5ebf8fd27358c0fb3a9f82
Anthony <tvierra30@outlook.com>
2024-09-27 20:55:15 -0400

    Initial Commit

---
+++ MyCoolApp/MyCoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoolAppApp.swift
+//  MyCoolApp
+//
+//  Created by Anthony on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCoolAppTests/MyCoolAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoolAppTests.swift
+//  MyCoolAppTests
+//
+//  Created by Anthony on 9/27/24.
+//
+
+import Testing
+@testable import MyCoolApp
+
+struct MyCoolAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
